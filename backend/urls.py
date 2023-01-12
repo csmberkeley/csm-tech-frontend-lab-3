27ea9c9805847837bed3f5bf605c12d17a03471c
Alec Li <smartspot2@gmail.com>
2023-01-12 00:26:31 -0800

    Add pytests

--- backend/urls.py
+++ backend/urls.py
@@ -1,12 +1,29 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path("users/", views.users),
-    path("sections/", views.sections),
-    path("sections/<int:section_id>/students/", views.section_students),
-    path("sections/<int:section_id>/details/", views.section_details),
-    path("students/<int:student_id>/details/", views.student_details),
-    path("students/<int:student_id>/attendances/", views.student_attendances),
-    path("students/<int:student_id>/drop/", views.student_drop),
+    path("users/", views.users, name="users"),
+    path("sections/", views.sections, name="sections"),
+    path(
+        "sections/<int:section_id>/students/",
+        views.section_students,
+        name="section-students",
+    ),
+    path(
+        "sections/<int:section_id>/details/",
+        views.section_details,
+        name="section-details",
+    ),
+    path(
+        "students/<int:student_id>/details/",
+        views.student_details,
+        name="student-details",
+    ),
+    path(
+        "students/<int:student_id>/attendances/",
+        views.student_attendances,
+        name="student-attendances",
+    ),
+    path("students/<int:student_id>/drop/", views.student_drop, name="student-drop"),
 ]
